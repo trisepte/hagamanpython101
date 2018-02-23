77ff1a3e71160adb3e38c405fbb6954fe06456ec
Sophia Coppola <spectralcanvas@gmail.com>
2018-02-22 19:14:12 -0500

    add colorama text

--- BMI/BMI.py
+++ BMI/BMI.py
@@ -1,8 +1,11 @@
 
 from gpiozero import RGBLED 
 from time import sleep
+from colorama import Fore
+from colorama import init
 
 led = RGBLED(16, 20, 21)
+init()
 
 print("Welcome to BMI Calculator!")
 
@@ -17,19 +20,19 @@ weight_kilo = weight*0.45
 bmi = weight_kilo / ( height_meter ** 2 )
 
 if bmi <= 18.5:
-    print("Your BMI is ", bmi," which means you are underweight. ")
+    print(Fore.RED + "Your BMI is ", bmi," which means you are underweight. ")
     led.color = (1, 0, 0) # red
     
 elif bmi > 18.5 and bmi < 25:
-    print("Your BMI is ", bmi," which means you are normal. ")
+    print(Fore.GREEN + "Your BMI is ", bmi," which means you are normal. ")
     led.color = (0, 1, 0) # green
 
 elif bmi > 25 and bmi < 30:
-    print("Your BMI is ", bmi," which means you are overweight. ")
+    print(Fore.YELLOW + "Your BMI is ", bmi," which means you are overweight. ")
     led.color = (1, 0.411765, 0) # orange
 
 elif bmi > 30:
-    print("Your BMI is ", bmi," which means you are obese. ")
+    print(Fore.RED + "Your BMI is ", bmi," which means you are obese. ")
     led.color = (1, 0, 0) # red
     
 else:
